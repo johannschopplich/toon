a34948ffdcc950f8a5dd5600f1b93f256e3328ea
Johann Schopplich <mail@johannschopplich.com>
2025-10-23 14:17:03 +0200

    chore: ignore tabs from linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,9 @@
 // @ts-check
 import antfu from '@antfu/eslint-config'
 
-export default antfu()
+export default antfu().append({
+  files: ['README.md'],
+  rules: {
+    'style/no-tabs': 'off',
+  },
+})
